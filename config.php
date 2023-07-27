58e2c07f481e0cf2bd459d9e07f2fcd0c113a6b4
IlonaAK <67979518+IlonaAK@users.noreply.github.com>
2023-07-27 19:52:20 +0200

    Add all files

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Configuration settings
+ */
+
+define('DB_HOST', 'blog.ilona-lelek.profesjonalnyprogramista.pl.ftp.dhosting.pl');
+define('DB_NAME', 'zeen4i_blogilon');
+define('DB_USER', 'eghoh4_blogilon');
+define('DB_PASS', 'laiz2Yoe6yai');
+
+define('SMTP_HOST', 'mail.example.com');
+define('SMTP_USER', 'user@example.com');
+define('SMTP_PASS', 'secret');
+
+define('SHOW_ERROR_DETAIL', true);
